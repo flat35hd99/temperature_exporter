30aa9af3a7ede7165351d7c2b2d5c38de25af589
flat35hd99 <flat35hd99@gmail.com>
2024-01-11 23:56:27 +0900

    go go go!

--- main.go
+++ main.go
@@ -13,41 +13,46 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func recordMetrics() {
+func recordTemperature() {
 	go func() {
-		for {
-			// Search /sys/bus/w1/devices/<device id>/w1_slave
-			// 1. Search device ids and then 2. get temperature from w1_slave of each device
-			dirEntries, err := os.ReadDir("/sys/bus/w1/devices")
-			if err != nil {
-				time.Sleep(recordInterval)
+		// Search /sys/bus/w1/devices/<device id>/w1_slave
+		// 1. Search device ids and then 2. get temperature from w1_slave of each device
+		dirEntries, err := os.ReadDir("/sys/bus/w1/devices")
+		if err != nil {
+			return
+		}
+		for _, dirEntry := range dirEntries {
+			if !dirEntry.IsDir() {
 				continue
 			}
-			for _, dirEntry := range dirEntries {
-				if !dirEntry.IsDir() {
-					continue
-				}
-				deviceId := dirEntry.Name()
+			deviceId := dirEntry.Name()
 
-				buf, err := os.ReadFile("/sys/bus/w1/devices/" + deviceId + "/w1_slave")
+			buf, err := os.ReadFile("/sys/bus/w1/devices/" + deviceId + "/w1_slave")
+			if err != nil {
+				continue
+			}
+			regexp := regexp.MustCompile(`t=(-?\d+)`)
+			matches := regexp.FindAllSubmatch(buf, -1)
+			for _, match := range matches {
+				// Convert types: byte -> string -> int -> float64
+				intTemperatureInMilliCelsius, err := strconv.Atoi(string(match[1]))
 				if err != nil {
 					continue
 				}
-				regexp := regexp.MustCompile(`t=(-?\d+)`)
-				matches := regexp.FindAllSubmatch(buf, -1)
-				for _, match := range matches {
-					// Convert types: byte -> string -> int -> float64
-					intTemperatureInMilliCelsius, err := strconv.Atoi(string(match[1]))
-					if err != nil {
-						continue
-					}
-					temperatureInCelsius := float64(intTemperatureInMilliCelsius) / 1000.0
+				temperatureInCelsius := float64(intTemperatureInMilliCelsius) / 1000.0
 
-					temperature.With(prometheus.Labels{
-						"id": deviceId,
-					}).Set(temperatureInCelsius)
-				}
+				temperature.With(prometheus.Labels{
+					"id": deviceId,
+				}).Set(temperatureInCelsius)
 			}
+		}
+	}()
+}
+
+func recordMetrics() {
+	go func() {
+		for {
+			recordTemperature()
 			time.Sleep(recordInterval)
 		}
 	}()
